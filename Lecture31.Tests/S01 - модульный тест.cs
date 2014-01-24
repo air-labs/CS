9418d777446d1ba05d46a4ba3e70f4aedbe8c603
okulovsky <yuri.okulovsky@gmail.com>
2014-01-24 18:30:13 +0600

    Добавил модульные тесты

---
+++ Lecture31.Tests/S01 - модульный тест.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+
+namespace TestProject
+{
+    [TestClass]
+    public class S01
+    {
+        void Test(double A, double B, double C,
+            int length, double x1, double x2)
+        {
+            var result = QuadricEquation.Solve(2, -4, -6);
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(result[0], 3);
+            Assert.AreEqual(result[1], -1);
+        }
+
+     
+    }
+}
---
+++ Lecture31.Tests/S03 - функциональные тесты.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+
+namespace TestProject
+{
+    [TestClass]
+    public class S03
+    {
+
+        [TestMethod]
+        public void FunctionalTest()
+        {
+            var rnd = new Random(1);
+            for (int i = 0; i < 100; i++)
+            {
+                var a = rnd.NextDouble() * 10;
+                var b = rnd.NextDouble() * 10;
+                var c = rnd.NextDouble() * 10;
+                var solution = QuadricEquation.Solve(a, b, c);
+                for (int j = 0; j < solution.Length; j++)
+                {
+                    Assert.AreEqual(0, a * solution[j] * solution[j] +
+                        b * solution[j] + c, 10e-6);
+                }
+            }
+        }
+    }
+}
