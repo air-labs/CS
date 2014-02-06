a2991e0703ba46e0faa421b97aba3de560b02d78
okulovsky <yuri.okulovsky@gmail.com>
2014-02-06 22:18:39 +0600

    Лекции на наследование и полиморфизм

---
+++ Lecture51/S02 - Множественная реализация интерфейсов.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace L2S02
+{
+
+	public class Complex : ICloneable
+	{
+		public double Real { get; set; }
+		public double Imaginary { get; set; }
+		public object Clone() { return new Complex { Imaginary=Imaginary, Real=Real}; }
+	}
+	
+	public class Angle : ICloneable, IComparable
+	{
+		public double Radian { get; set; }
+		public object Clone() { return new Angle { Radian=Radian }; }
+		public int CompareTo(object obj)
+		{
+			return Radian.CompareTo( (obj as Angle).Radian);
+		}
+	}
+			                        
+	class Program
+	{
+		public static void MainX()
+		{
+			var complex=new Complex();
+			Console.WriteLine (complex is ICloneable);
+			Console.WriteLine (complex is IComparable);
+			
+			var angle=new Angle();
+			Console.WriteLine (angle is ICloneable);
+			Console.WriteLine (angle is IComparable);
+			
+            var comparableAngle1 = angle as IComparable;
+            var comparableAngle2 = (IComparable)angle;
+            var comparableCOmplex1 = complex as IComparable;
+            var comparableComplex2 = (IComparable)complex;
+		}
+	}
+}
---
+++ Lecture51/S03 - Свойства в интерфейсе.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace L2S03
+{
+	public interface IPrintable
+	{
+		void Print();	
+        //Интерфейс может содержать свойства, поскольку свойства - это методы
+		ConsoleColor Color { get; }
+	}
+	
+	public class Person : IPrintable
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public void Print()
+		{
+			Console.WriteLine("{0} ({1} y.o)",Name,Age);
+		}
+		public ConsoleColor Color { get { return ConsoleColor.Red; } }
+	}
+	
+	class Program
+	{
+		static void PrintWithColor(IPrintable prn)
+		{
+			Console.ForegroundColor=prn.Color;
+			prn.Print ();
+		}
+		
+		public static void MainX()
+		{
+			PrintWithColor (new Person{ Name="Smith", Age=20 });		
+		}
+	}
+}
